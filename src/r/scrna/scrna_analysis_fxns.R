4c94c5494daa0ad57a19a1ced64696af6fbf4c87
dbgoodman <ec2-user@ip-172-31-37-77.us-west-2.compute.internal>
2020-11-04 18:15:29 +0000

    start of new functionalizations, cd4 data.

--- src/r/scrna/scrna_analysis_fxns.R
+++ src/r/scrna/scrna_analysis_fxns.R
@@ -363,6 +363,44 @@ comp_car_volcano <- function(obj_subset, ident.1, ident.2, logfc.thresh=0.2, min
 
 ####### UMAP PLOTTING
 
+get_cluster_pct_plot <- function(this_subset, cluster_col_name) {
+  cluster_pct_plot <- ggplot(
+      data.table(this_subset@meta.data)[
+        CD4v8 != 'intermediate', .N, 
+        by=c('car', cluster_col_name, 'CD4v8', 'k_type')][,
+          list(cluster=get(cluster_col_name), t_type=CD4v8, frac=N/sum(N)), 
+          by=c('car', 'CD4v8', 'k_type')]) + 
+    geom_bar(aes_string(x='cluster', y='frac', fill='frac'), stat='identity', color='grey50') +
+    scale_fill_distiller(palette='YlGnBu', direction=1) +
+    facet_grid(k_type+t_type~car) + 
+    theme_bw() +
+    coord_flip()
+  
+  return(cluster_pct_plot)
+}
+
+get_cluster_enrich_plot <- function(this_subset, cluster_col_name, max_log2_enrich=2, hide=c('KLRG1','Untransduced')) {
+
+  enrich_data <- data.table(this_subset@meta.data)[
+    CD4v8 != 'intermediate' & !(car %in% hide) , .N, 
+    by=c('car', cluster_col_name, 'CD4v8', 'k_type')][,
+      list(cluster=get(cluster_col_name), t_type=CD4v8, frac=N/sum(N)), 
+      by=c('car', 'CD4v8', 'k_type')][,
+        rel_frac := log2(frac/mean(frac)), by=c('CD4v8','k_type','cluster')]
+  
+  enrich_data[, rel_frac_disp := ifelse(abs(rel_frac) > max_log2_enrich, 
+    sign(rel_frac)*max_log2_enrich,
+    rel_frac)]
+  
+  ggplot(enrich_data) + 
+    geom_bar(aes_string(x='cluster', y='frac', fill='rel_frac_disp'), stat='identity', color='grey50') +
+    scale_fill_distiller('Log2 Enrichment\nvs mean CAR', palette='PuOr', direction=1,
+      limits=c(-max_log2_enrich, max_log2_enrich)) +
+    facet_grid(k_type+t_type~car) + labs(x='Log2 enrichment vs mean car') +
+    theme_bw() +
+    coord_flip()
+}
+  
 umap_plot <- function(this_subset, plot_title_text="", assay, cluster_resolution=NULL, cluster_name=NULL, 
     reduction='umap', feat_logfc_thresh=0.25, feat_min_pct=0.5) {
   
@@ -391,17 +429,7 @@ umap_plot <- function(this_subset, plot_title_text="", assay, cluster_resolution
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
   
   
-  cluster_pct_plot <- ggplot(
-      data.table(this_subset@meta.data)[
-        CD4v8 != 'intermediate', .N, 
-        by=c('car', cluster_col_name, 'CD4v8', 'k_type')][,
-          list(cluster=get(cluster_col_name), t_type=CD4v8, frac=N/sum(N)), 
-          by=c('car', 'CD4v8', 'k_type')]) + 
-    geom_bar(aes_string(x='cluster', y='frac', fill='frac'), stat='identity', color='grey50') +
-    scale_fill_distiller(palette='YlGnBu', direction=1) +
-    facet_grid(k_type+t_type~car) + 
-    theme_bw() +
-    coord_flip()
+  cluster_pct_plot <- get_cluster_pct_plot(this_subset, cluster_col_name)
   
   plot_title <- ggdraw() + 
     draw_label(
@@ -852,3 +880,213 @@ load_vision_data <- function(
   saveRDS(vis, vision_rds_file)
   return(vis)
 }
+
+plot_sig_by_car <- function(seurat_obj, vis, signature, x_lab, assay='RNA') {
+
+  DefaultAssay(seurat_obj) <- assay
+  sig_scores <- getSignatureScores(vis)
+
+  sig.data <- data.table(
+    cbind(seurat_obj@meta.data, sig_scores))[, 
+      car := factor(
+        car,
+        levels=c('CD28','4-1BB','BAFF-R','TACI','CD40','TNR8','KLRG1','Zeta','Untransduced'),
+        labels=c(car_order,'Untransduced'))]
+  
+  plot <- ggplot(sig.data) +
+    geom_boxplot(aes_string(x=signature, y='car', fill='car', color='car'), alpha=0.7, outlier.shape=NA) + 
+    scale_fill_manual(values=c(car_colors, Untransduced='grey80')) +
+    scale_color_manual(values=c(car_colors, Untransduced='grey80')) +
+    theme_bw() + theme(panel.grid=element_blank(), text=element_text(family="Arial Unicode MS")) + labs(x=x_lab)
+  
+  return(plot)
+}
+
+plot_sig_by_cluster <- function(seurat_obj, vis, signature, x_lab, cluster_col='VISION_Clusters') {
+
+  DefaultAssay(seurat_obj) <- 'RNA'
+  sig_scores <- getSignatureScores(vis)
+
+  sig.data <- data.table(
+    cbind(seurat_obj@meta.data, sig_scores))
+  
+  plot <- ggplot(sig.data) +
+  geom_boxplot(aes_string(x=signature, y=cluster_col, fill=cluster_col, color=cluster_col), alpha=0.7, outlier.shape=NA) + 
+  theme_bw() + theme(panel.grid=element_blank(), text=element_text(family="Arial Unicode MS")) + labs(x=x_lab)
+  
+  return(plot)
+}
+
+car_feature_dotplot <- function(seurat_obj, features, assay='RNA') {
+
+  #Missing features?
+  #print(setdiff(features, intersect(features, rownames(seurat_obj@assays$RNA@scale.data))))
+  
+  DefaultAssay(seurat_obj) <- assay
+  
+  feature_dt <- melt(
+    data.table(cbind(t(seurat_obj@assays[[assay]]@scale.data[features,]), seurat_obj@meta.data))[
+      !(car %in% c('K_only','KLRG1','Untransduced'))], 
+    measure.vars=features, variable.name='gene')[,
+      `:=`(pct_exp_global=sum(value > 0)/.N, mean_exp_global=mean(value[value > 0])), by=c('CD4v8','gene')][,
+        list(pct_exp=(sum(value > 0)/.N), mean_exp_fldch=mean(value[value > 0])/mean_exp_global), 
+        by=c('car','CD4v8','gene')]
+  
+  return(ggplot(feature_dt[CD4v8 != 'intermediate']) + 
+    geom_point(
+      aes(x=factor(car, levels=c(car_order,'4-1BB', 'Zeta')), 
+          y=factor(gene, levels=features), 
+          color=mean_exp_fldch, 
+          size=pct_exp)) + 
+    facet_grid(~CD4v8) + 
+    scale_color_distiller('Relative Expression Change\nfrom Mean',direction=1, palette='PiYG') + 
+    theme_minimal() + 
+    theme(panel.grid=element_blank()) + 
+    scale_size_continuous('% Cells Expressing',range=c(2,8)) +
+    labs(x='CAR',y='Gene'))
+
+}
+
+signature_plot <- function(seurat_obj, vis_obj, this_signature) {
+
+  this_sig_geneset <- vis_obj@SigGeneImportance[[this_signature]]
+  ordered_filtered_sig_geneset <- sort(
+    this_sig_geneset[intersect(VariableFeatures(seurat_obj, assay='RNA'), names(this_sig_geneset))],
+    decreasing=T)
+  
+  gene_score <- ggplot(data.table(gene=names(ordered_filtered_sig_geneset), score=(ordered_filtered_sig_geneset))) + 
+    geom_tile(aes(x='score', y=reorder(gene, score), fill=score)) + 
+    scale_fill_distiller(
+      palette='RdBu', 
+      limits=c(-max(abs(ordered_filtered_sig_geneset)), max(abs(ordered_filtered_sig_geneset)))) + 
+    labs(x='',y='') + theme_minimal() + 
+    theme(panel.grid=element_blank()) + 
+    scale_x_discrete(expand=c(0,0))
+  
+  plot_grid(
+    DoHeatmap(seurat_obj,
+    features=names(ordered_filtered_sig_geneset), 
+    assay='RNA', group.by = 'VISION_Clusters'),
+    plot_grid(
+        cluster_car_pct_plot(seurat_obj, 'VISION_Clusters'),
+        plot_sig_by_car(seurat_obj, vis_obj, this_signature, x_lab=this_signature), 
+        plot_sig_by_cluster(seurat_obj, vis_obj, this_signature, x_lab=this_signature), ncol=1),
+    ncol=2,
+    align = 'h', axis = 'tb')
+}
+
+### NEW SCT/CLUSTERING Functions
+
+# SCTransform and UMAP of a subset of cells
+sct_umap <- function(seurat_obj, pct.mt.cutoff=13, cluster_res=1.3, title="") {
+  
+  plots = list()
+  
+  # remove high MT-RNA cells (dying/dead?)
+  DefaultAssay(seurat_obj) <- 'RNA'
+  seurat_obj <- PercentageFeatureSet(seurat_obj, pattern = "^MT-", col.name = "percent.mt")
+  plots$mt.cutoff <- FeatureScatter(
+    seurat_obj, feature1 = "nCount_RNA", feature2 = "percent.mt") + 
+    geom_hline(yintercept=pct.mt.cutoff)
+  seurat_obj <- subset(seurat_obj, subset=(percent.mt < pct.mt.cutoff))
+  
+  # get cell cycle scores
+  DefaultAssay(seurat_obj) <- 'RNA'
+  s.genes <- cc.genes$s.genes
+  g2m.genes <- cc.genes$g2m.genes
+  #https://satijalab.org/seurat/v3.1/cell_cycle_vignette.html
+  seurat_obj <- CellCycleScoring(seurat_obj, s.features = s.genes, g2m.features = g2m.genes, set.ident = TRUE)
+  seurat_obj <- RunPCA(seurat_obj, reduction.name = 'cc_pca')
+  
+  #SCTransform for RNA
+  DefaultAssay(seurat_obj) <- 'RNA'
+  seurat_obj <- SCTransform(seurat_obj, 
+    vars.to.regress = c("S.Score", "G2M.Score"), verbose = FALSE)
+  seurat_obj <- RunPCA(seurat_obj, reduction.name = 'rna_pca')
+  
+  #SCTransform for ADT
+  DefaultAssay(seurat_obj) <- 'ADT'
+  seurat_obj <- SCTransform(
+    seurat_obj, assay='ADT', new.assay.name = 'SCT_ADT', 
+    vars.to.regress = c("S.Score", "G2M.Score"))
+  seurat_obj <- RunPCA(
+    seurat_obj, features = rownames(seurat_obj[["SCT_ADT"]]), 
+    nfeatures.print = 10, reduction.name='adt_pca')
+  
+  #WNN graph for both PCAs
+  seurat_obj <- FindMultiModalNeighbors(
+    seurat_obj, reduction.list = list("rna_pca", "adt_pca"), 
+    dims.list = list(1:30, 1:18), modality.weight.name = "RNA.weight"
+  )
+  
+  cluster_resolution <- 1.3
+  cluster_name <- paste0('wsnn_res.',as.character(cluster_resolution))
+  
+  seurat_obj <- RunUMAP(seurat_obj, nn.name = "weighted.nn", reduction.name = "wnn.umap", reduction.key = "wnnUMAP_")
+  seurat_obj <- FindClusters(seurat_obj, graph.name = "wsnn", algorithm = 3, resolution = cluster_resolution, verbose = FALSE)
+  
+  pl_clusters <- DimPlot(seurat_obj, reduction = 'wnn.umap', label = TRUE,
+    repel = TRUE, label.size = 2.5) + NoLegend()
+  pl_cars <- DimPlot(seurat_obj, reduction = 'wnn.umap', group.by = 'car', label = TRUE,
+    repel = TRUE, label.size = 2.5)
+  pl_ktype <- DimPlot(seurat_obj, reduction = 'wnn.umap', group.by = 'k_type', label = TRUE,
+    repel = TRUE, label.size = 2.5) + NoLegend()
+  pl_ccphase <- DimPlot(seurat_obj, reduction = 'wnn.umap', group.by = 'Phase', label = TRUE,
+    repel = TRUE, label.size = 2.5) + NoLegend()
+  plots$combined_umap <- pl_clusters + pl_ktype + pl_ccphase + pl_cars
+  
+  DefaultAssay(seurat_obj) <- 'SCT_ADT'
+  adt_umap_plots <- umap_plot(seurat_obj, 
+    plot_title_text=paste(title,"ADT WNN"), 
+    assay='SCT_ADT', cluster_name=cluster_name, reduction= 'wnn.umap')
+  
+  DefaultAssay(seurat_obj) <- 'SCT'
+  rna_umap_plots <- umap_plot(seurat_obj, 
+    plot_title_text=paste(title,"RNA WNN"), assay='SCT', cluster_name=cluster_name, reduction= 'wnn.umap')
+
+  plots$adt_umap_plots <- adt_umap_plots$umap_plots
+  plots$rna_umap_plots <- rna_umap_plots$umap_plots
+  
+  return(list(obj=seurat_obj, plots=plots, adt_markers= adt_umap_plots$marker_genes, rna_markers= rna_umap_plots$marker_genes))
+}
+
+# Map clusters from one object to another
+map_clusters <- function(from_obj, to_obj, from_cluster_name, to_cluster_name) {
+  from_clust_data <- data.table(from_obj@meta.data, keep.rownames=T)[,
+  list(rn, clust= get(from_cluster_name))][
+    data.table(to_obj@meta.data, keep.rownames=T), on=('rn')]
+
+  to_obj@meta.data[[to_cluster_name]] <- from_clust_data$clust
+  return(to_obj)
+}
+
+# Map activation % to each cluster (CD19+ vs baseline), of CAR clusters, (not KLRG1/Untransduced)
+map_clust_act <- function(seurat_obj) {
+  car_clust_data <- data.table(
+  seurat_obj@meta.data)[, list(n_clust_car= .N), 
+  by=.(seurat_clusters, car, k_type)][, 
+    pct_clust_car := n_clust_car/sum(n_clust_car), 
+    by=.(car, k_type)][,
+      cd19_clust_car_l2fc := log2(pct_clust_car[k_type == 'cd19+']/pct_clust_car[k_type == 'baseline']), 
+      by=.(seurat_clusters, car)][, 
+        cd19_clust_act_l2fc := mean(cd19_clust_car_l2fc[!(car %in% c('KLRG1','Untransduced'))], na.rm=T), 
+        by=seurat_clusters]
+  
+  car_clust_data[, pct_clust_car_max := max(pct_clust_car), by=.(seurat_clusters, car)]
+  car_clust_data[, is_act := max(pct_clust_car) > pct_clust_car[k_type == 'baseline'], by=.(seurat_clusters, car)]
+  car_clust_data[is.na(is_act), is_act := T]
+  car_clust_data[, is_act_avg := sum(is_act) > length(unique(car)), by=.(seurat_clusters)]
+
+  act_plot <- ggplot(car_clust_data) + 
+    geom_point(aes(x=seurat_clusters, y=car, fill=cd19_clust_car_l2fc, size=pct_clust_car_max), shape=21) + 
+    scale_fill_distiller(palette='PRGn', direction=1) + scale_size_area(max_size=12) + theme_minimal() + 
+    facet_grid(~is_act_avg, scales='free_x')
+
+  activation_colors <- ggplot_build(
+    ggplot(unique(car_clust_data[, list(cd19_clust_act_l2fc, seurat_clusters)])) + 
+    geom_tile(aes(x=seurat_clusters, y='1', fill=cd19_clust_act_l2fc)) + 
+    scale_fill_distiller(palette='PRGn', direction=1))$data[1][[1]][c('fill','x')]
+  
+  return(list(car_clust_data=car_clust_data, act_plot=act_plot, activation_colors=activation_colors))
+}
+
